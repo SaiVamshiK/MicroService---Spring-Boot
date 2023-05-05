030f5c2beaf0584f8498488bd76730f9ca0ffe34
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-05-05 18:15:20 +0530

    Section 2 : created basic project

---
+++ Section2/spring-boot-section2/src/main/java/com/section2/springbootsection2/SpringBootSection2Application.java
@@ -0,0 +1,13 @@
+package com.section2.springbootsection2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSection2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSection2Application.class, args);
+	}
+
+}
---
+++ Section2/spring-boot-section2/src/test/java/com/section2/springbootsection2/SpringBootSection2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section2.springbootsection2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSection2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
