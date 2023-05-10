cb62850e52feec97617e09b3d32aace9e317f04d
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-05-10 18:22:30 +0530

    Section 4 : injecting the service

--- Section2/spring-boot-section2/src/main/java/com/section2/springbootsection2/controller/EmployeeController.java
+++ Section2/spring-boot-section2/src/main/java/com/section2/springbootsection2/controller/EmployeeController.java
@@ -1,5 +1,8 @@
 package com.section2.springbootsection2.controller;
 
+import com.section2.springbootsection2.servicelayer.EmployeeService;
+import com.section2.springbootsection2.servicelayer.impl.EmployeeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/employees")
 public class EmployeeController {
 
+    @Autowired
+    private EmployeeService employeeService;
+    // Here we are injecting the employee service interface, since the interface has only 1 implementation, it will use that
 
 
 }
--- Section2/spring-boot-section2/src/main/java/com/section2/springbootsection2/servicelayer/impl/EmployeeServiceImpl.java
+++ Section2/spring-boot-section2/src/main/java/com/section2/springbootsection2/servicelayer/impl/EmployeeServiceImpl.java
@@ -1,6 +1,11 @@
 package com.section2.springbootsection2.servicelayer.impl;
 
 import com.section2.springbootsection2.servicelayer.EmployeeService;
+import org.springframework.stereotype.Service;
 
+// @Service is an annotation we use, it signifies that it is a service component.
+// It is same as @Component
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
+
 }
