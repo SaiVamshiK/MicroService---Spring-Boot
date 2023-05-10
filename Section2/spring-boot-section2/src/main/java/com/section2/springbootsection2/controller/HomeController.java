38085a628b2483ab9762498a9698e6741eab7c80
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-05-10 17:49:09 +0530

    Section 4 : using REST controller

--- Section2/spring-boot-section2/src/main/java/com/section2/springbootsection2/controller/HomeController.java
+++ Section2/spring-boot-section2/src/main/java/com/section2/springbootsection2/controller/HomeController.java
@@ -3,13 +3,17 @@ package com.section2.springbootsection2.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 // @Controller is the annotation, that indicates to spring boot that
 // it serves the role of a controller.
 // @ResponseBody annotation tells a controller that the object returned
 // is automatically serialized into JSON and passed back into the HttpResponse object.
-@Controller
-@ResponseBody
+//@Controller
+//@ResponseBody
+// Using @RestController instead of both @Controller and @ResponseBody
+// The @RestController will have the above 2 annotations defined.
+@RestController
 public class HomeController {
 
     // All request with path "/" will come here.
