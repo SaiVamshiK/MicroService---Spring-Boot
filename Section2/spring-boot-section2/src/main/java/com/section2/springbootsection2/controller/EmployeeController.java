106af9e45c24a0676c50c837725868314c035d2f
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-05-10 18:16:56 +0530

    Section 4 : created employee controller

---
+++ Section2/spring-boot-section2/src/main/java/com/section2/springbootsection2/controller/EmployeeController.java
@@ -0,0 +1,13 @@
+package com.section2.springbootsection2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    
+
+}
